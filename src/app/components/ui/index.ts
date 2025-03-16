4bb0a9727ec0c66781d15800c1ea479ab54d3eaf
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2025-03-16 12:32:47 +0900

    export

--- src/app/components/ui/index.ts
+++ src/app/components/ui/index.ts
@@ -1,4 +1,8 @@
 export { default as Header } from "./Header";
-export { default as TypingArea } from "./TypingArea";
 export { default as Button } from "./Button";
 export { default as ButtonGroup } from "./Button";
+export { default as Modal } from "./Modal";
+export { default as GameScreen } from "./GameScreen";
+export { default as TypingArea } from "./TypingArea";
+export { default as ExampleDisplay } from "./ExampleDisplay";
+export { default as PlayScreen } from "./PlayScreen";
